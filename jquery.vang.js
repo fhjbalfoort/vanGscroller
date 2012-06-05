7880e15e7757a6375cdce744a18226c12aeed48e
Felix Balfoort <fhjbalfoort@gmail.com>
2012-06-05 15:33:23 +0200

    code refactoring

--- jquery.vang.js
+++ jquery.vang.js
@@ -1,14 +1,37 @@
 (function( $ ) {
-	var _plugin = this;
-	_plugin.options = {
-		divs :{
-			content : '.content',
-			scroll_bar  : 'scrollbar',
-			scroll_grab : 'scrollgrab'
+	var _plugin = $.extend(this,  {
+		update : [],
+		options : {
+			divs :{
+				content : '.content',
+				scroll_bar  : 'scrollbar',
+				scroll_grab : 'scrollgrab'
+			},
+			update: true
 		},
-		update: true
-	};
-	_plugin.update = [];
+		setScroll : function(self){
+			if($( '> '+ self.options.divs.content, self).outerHeight() < self.currentScrollHeight){
+				var scrollPercent = $( '> '+self.options.divs.content, self).outerHeight() /  self.currentScrollHeight;
+				var scrollbarHeight = Math.round( $('> ' + self.options.divs.content , self).height()  * scrollPercent) ;
+				$('> .' + self.options.divs.scroll_bar,self).show().find('> .' + self.options.divs.scroll_grab).height( scrollbarHeight + 'px');
+
+				$('> .' + self.options.divs.scroll_bar + ' > .' + self.options.divs.scroll_grab, self).unbind('drag').drag(function( ev, dd ){
+					if(dd.offsetY >= dd.startY-2 && dd.offsetY-dd.startY <= ($('> ' + self.options.divs.content , self).outerHeight()  )){
+						$('> ' + self.options.divs.content , self).scrollTop((dd.offsetY-dd.startY)/ scrollPercent);
+					}
+				});
+
+				$('> .' + self.options.divs.scroll_bar + ' > .' + self.options.divs.scroll_grab,self).css('top', Math.round( $(self.options.divs.content, self).scrollTop() * scrollPercent)+'px');
+				$('> ' + self.options.divs.content, self).unbind('scroll').scroll(
+					function(){
+						$('> .' + self.options.divs.scroll_bar + ' > .' + self.options.divs.scroll_grab,self).css('top', Math.round( $(this).scrollTop() * scrollPercent)+'px');
+					});
+			} else {
+				$('> .' + self.options.divs.scroll_bar, self).hide();
+			}
+		}
+	});
+
 	setInterval(function(){
 		for(var i in _plugin.update ){
 			if(_plugin.update[i].currentScrollHeight != $('> '+_plugin.update[i].options.divs.content, _plugin.update[i]).prop('scrollHeight')){
@@ -17,31 +40,6 @@
 			}
 		}
 	}, 400);
-	_plugin.setScroll = function(self){
-		if($( '> '+ self.options.divs.content, self).outerHeight() < self.currentScrollHeight){
-			var scrollPor = $( '> '+self.options.divs.content, self).outerHeight() /  self.currentScrollHeight;
-			var scrollbarHeight = Math.round( $('> ' + self.options.divs.content , self).height()  * ($('> ' + self.options.divs.content, self).outerHeight() /  self.currentScrollHeight) ) ;
-			$('> .' + self.options.divs.scroll_bar,self).show().find('> .' + self.options.divs.scroll_grab).height( scrollbarHeight + 'px');
-
-
-
-			$('> .' + self.options.divs.scroll_bar + ' > .' + self.options.divs.scroll_grab, self).unbind('drag').drag(function( ev, dd ){
-				if(dd.offsetY >= dd.startY-2 && dd.offsetY-dd.startY <= ($('> ' + self.options.divs.content , self).outerHeight()  )){
-					$('> ' + self.options.divs.content , self).scrollTop((dd.offsetY-dd.startY)/scrollPor);
-				}
-			});
-
-
-			$('> .' + self.options.divs.scroll_bar + ' > .' + self.options.divs.scroll_grab,self).css('top', Math.round( $(self.options.divs.content, self).scrollTop() * scrollPor)+'px');
-			$('> ' + self.options.divs.content, self).unbind('scroll').scroll(
-				function(){
-					$('> .' + self.options.divs.scroll_bar + ' > .' + self.options.divs.scroll_grab,self).css('top', Math.round( $(this).scrollTop() * scrollPor)+'px');
-				});
-		} else {
-			$('> .' + self.options.divs.scroll_bar, self).hide();
-
-		}
-	}
 		
 	$.fn.vanG = function() {
 		return this.each(function(defaults) {
